ce96d8eaa9b5df8b5e434994f16be147752ab3af
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-05-07 15:44:36 +0900

    new TableController and TableView

--- NewSimpleTable03/ViewController.swift
+++ NewSimpleTable03/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var animal = ["cat", "penguin", "bird", "cow", "monkey", "dog"]
+    var detailAnimal = ["고양이", "펭귄", "새", "소", "원숭이", "강아지"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        //코드로 타이틀 추가하는 방법
+        title.self = "동물농장"
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+       let myCell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+       let myRow = indexPath.row
+        
+       myCell.textLabel?.text = animal[myRow]
+       myCell.detailTextLabel?.text = detailAnimal[myRow]
+        
+       myCell.imageView?.image = UIImage(named: animal[myRow])
+        return myCell
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+         return animal.count
     }
 
 
